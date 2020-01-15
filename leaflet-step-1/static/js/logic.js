b6d38682aa5d1f704ea9448a4c6bb0674c3c9a86
wtcarnes <51512865+wtcarnes@users.noreply.github.com>
2020-01-15 15:47:51 -0600

    Update logic.js

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -13,7 +13,7 @@ legend.onAdd = function () {
 
     var div = L.DomUtil.create('div', 'info legend'),
         grades = [1, 2, 3, 4, 5],
-
+        labels = [];
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML += '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
